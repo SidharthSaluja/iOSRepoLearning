423464057ed1ca6d49ea810eaa5bb84605137e6d
Sidharth Saluja <67796130+SidharthSaluja@users.noreply.github.com>
2023-07-20 14:41:54 -0600

    Add files via upload

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseCore
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    let db = Firestore.firestore()
+    var messages: [Message] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        title = K.appName
+        navigationItem.hidesBackButton = true
+        loadMessages()
+    }
+    func loadMessages() {
+        
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            if let e = error {
+                print(e)
+            }
+            else{
+                if let snapShotDocuments = querySnapshot?.documents {
+                    for doc in snapShotDocuments {
+                        let data = doc.data()
+                        if let Msender = data[K.FStore.senderField] as? String, let Mbody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: Msender, body: Mbody)
+                            self.messages.append(newMessage)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error {
+                   print(e)
+                }
+                else {
+                    print("Successfully saved data")
+                }
+            }
+        }
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = message.body
+        if message.sender == Auth.auth().currentUser?.email {
+            print(message.sender)
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
+        return cell
+    }
+    
+    
+}
